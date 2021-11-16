968be738debd04c8e0accf0802529f40a25ac145
nikawamikan <wkwktktkkrkr@outlook.jp>
2021-11-16 23:30:42 +0900

    retweeted判定がされていなかったのでとりあえず修正

--- index.js
+++ index.js
@@ -4,25 +4,23 @@ const yaml = require('js-yaml');
 const fs = require('fs');
 
 const conf = yaml.load(fs.readFileSync('config.yml', 'utf8'));
-
 const webhook = new WebhookClient(conf.webhook);
-
 const client = new Twitter(conf.twitter);
 
 const main = async() =>{
     const stream = await client.stream('statuses/filter',{ 'track': '#ふぃぼ鯖' });
-    stream.on('data', async data =>{
+    stream.on('data', async data => {
+        // console.log(data.retweeted_status);
+        if(!data.retweeted_status){
+          const url = `https://twitter.com/${data.user.screen_name}/status/${data.id_str}`;
 
-        const url = `https://twitter.com/${data.user.screen_name}/status/${data.id_str}`;
-
-        await webhook.send({
-            content: url, // 普通にコンテンツ
-            username: data.user.name, // WebHookのアイコンをオーバーライド
-            avatarURL: data.user.profile_image_url_https, // WebHookの名前をオーバーライド
-        });
-        
+          await webhook.send({
+              content: url, // 普通にコンテンツ
+              username: data.user.name, // WebHookのアイコンをオーバーライド
+              avatarURL: data.user.profile_image_url_https, // WebHookの名前をオーバーライド
+          });
+        }
     });
-
     stream.on('error', async error =>{
         await console.log(error);
     });
